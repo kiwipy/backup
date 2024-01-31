49d2294b14367bdbb7e0ef18758bc36e5f2a8710
William Andersson <william.andersson@outlook.com>
2024-01-31 14:23:03 +0100

    New more comprehensive version

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+##### Script installation instructions #####
+DEPENDENCIES=("bc rsync")
+# Copy scripts
+install -v -C -m 775 -o root backup.sh /usr/bin/toolbox-backup
+# Copy configurations
+if [ ! -f "/etc/toolbox-backup.cfg" ]; then
+	install -v -C -m 666 -o root backup.cfg /etc/toolbox-backup.cfg
+else
+	echo "File /etc/toolbox-backup.cfg already exists, skipping..."
+fi
+
+##### Resolve dependencies (DON'T TOUCH) ######
+DNF="dnf -y install"
+APT="apt-get -y install"
+ZYP="zypper -n install"
+PKG_MGR=""
+if command -v zypper &> /dev/null;then
+	PKG_MGR=$ZYP
+elif command -v apt-get &> /dev/null;then
+	PKG_MGR=$APT
+elif command -v dnf &> /dev/null;then
+	PKG_MGR=$DNF
+else
+	echo "No package-manager found!"
+	echo "Please install dependencies: ${DEPENDENCIES[@]}"
+	exit 0
+fi
+for DEP in ${DEPENDENCIES[@]};do
+	if ! command -v $DEP &> /dev/null;then
+		$PKG_MGR ${DEPENDENCIES[@]}
+	fi
+done
+
+echo "Done."
