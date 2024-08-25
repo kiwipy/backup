6efdbef4e24c2938426e81d7ef04015705543584
William Andersson <william.andersson@outlook.com>
2024-08-25 14:45:56 +0200

    7.1.1
    - cleanup_dest(); Rewrite.
    - remove_backup(); File check after remove.

--- bakup.sh
+++ bakup.sh
@@ -6,17 +6,17 @@
 # Website:     https://github.com/william-andersson
 # License:     GPL  
 #
-VERSION=7.1.0
+VERSION=7.1.1
 
-if [[ $EUID -ne 0 ]]; then
+if [[ $EUID -ne 0 ]];then
     echo -e "\nThis script must be run as root!"
     exit 1
 fi
 
 # Load config file if exists.
-if [ ! -f "/usr/local/etc/bakup.cfg" ];then
-    if [ "$STARTED_BY_SYSTEMD" != yes ]; then
-        if [ "$1" != "--setup" ];then
+if [[ ! -f "/usr/local/etc/bakup.cfg" ]];then
+    if [[ "$STARTED_BY_SYSTEMD" != yes ]];then
+        if [[ "$1" != "--setup" ]];then
             echo -e "\n*** No config file found, Run --setup first! ***\n"
         fi
     else
@@ -102,7 +102,7 @@ setup(){
 
     HASH=""
 
-    if [ ! -d "/usr/local/etc" ];then
+    if [[ ! -d "/usr/local/etc" ]];then
         mkdir -p /usr/local/etc
     fi
     touch /usr/local/etc/bakup.cfg
@@ -117,11 +117,11 @@ search_for_backups(){
     #
     # Look for existing backups to use as starting point
     #
-    if [ $(ls -l $DEST | grep index | wc -l) -eq 1 ];then
+    if [[ $(ls -l $DEST | grep index | wc -l) -eq 1 ]];then
         CURRENT=$(ls -t $DEST | grep -m 1 index | cut -f1 -d".")
         PREV=$CURRENT
         echo "Found existing backup. Added: [$CURRENT]"
-    elif [ $(ls -l $DEST | grep index | wc -l) -eq 2 ];then
+    elif [[ $(ls -l $DEST | grep index | wc -l) -eq 2 ]];then
         CURRENT=$(ls -t $DEST | grep -m 1 index | cut -f1 -d".")
         PREV=$(ls -tr $DEST | grep -m 1 index | cut -f1 -d".")
         echo "Found existing backups. Added: [$CURRENT] and [$PREV]"
@@ -134,7 +134,7 @@ search_for_backups(){
 auto_timer(){
     local NEW_TIMER=$1
     local current_timer=$(cat /etc/systemd/system/bakup-auto.timer | grep OnCalendar)
-    if [ "$NEW_TIMER" == "off" ];then
+    if [[ "$NEW_TIMER" == "off" ]];then
         sed -i 's:'$current_timer':'OnCalendar=$NEW_TIMER':' /etc/systemd/system/bakup-auto.timer
         systemctl disable bakup-auto.timer --now 2>/dev/null
         echo "auto timer set: [$NEW_TIMER]"
@@ -152,7 +152,7 @@ set_limit(){
         echo "Input value not integer!"
         exit 1
     fi
-    if [ "$1" == "0" ];then
+    if [[ "$1" == "0" ]];then
         LIMIT=0
     else
         LIMIT=$(($(($1*1024))*1024))
@@ -164,7 +164,7 @@ set_thresh(){
         echo "Input value not integer!"
         exit 1
     fi
-    if [ $1 -ge 10 ] && [ $1 -lt 100 ];then
+    if [[ $1 -ge 10 ]] && [[ $1 -lt 100 ]];then
         THRESH=$1
     else
         echo "Invalid thresh value (10-99)"
@@ -182,29 +182,29 @@ set_max(){
 
 set_path(){
     # Called by --set-target, --set-dest & --migrate
-    if [ ! $2 ];then
+    if [[ ! $2 ]];then
         echo "Missing path!"
         exit 2
     fi
     
-    if [ "$1" == "--set-target" ];then
+    if [[ "$1" == "--set-target" ]];then
         TARGET=$2
-        if [ ! -d "$TARGET" ];then
+        if [[ ! -d "$TARGET" ]];then
             echo "No such directory! [$TARGET]"
             exit 2
         else
             echo "Target directory set [$TARGET]"
         fi
-    elif [ "$1" == "--set-dest" ];then
+    elif [[ "$1" == "--set-dest" ]];then
         DEST=$2
-        if [ ! -d "$DEST" ];then
+        if [[ ! -d "$DEST" ]];then
             mkdir -pv $DEST
         fi
         echo "Destination directory set [$DEST]"
-    elif [ "$1" == "--migrate" ];then
+    elif [[ "$1" == "--migrate" ]];then
         mkdir -pv ${2%/}
         mv -v $DEST/* -t ${2%/}/
-        if [ "$?" == "0" ];then
+        if [[ "$?" == "0" ]];then
             rm -rv $DEST
             DEST=$2
             echo "Backup directory migrated to [$DEST]"
@@ -218,12 +218,12 @@ set_path(){
 list_backups(){
     echo "### Available backups ###"
     echo "$PREV - $(du -sh $DEST/$PREV.tgz | awk '{print $1}') *"
-    for bak in $(cat $DEST/$PREV.index); do
+    for bak in $(cat $DEST/$PREV.index);do
         echo "$bak - $(du -sh $DEST/$bak.tgz | awk '{print $1}')"
     done
-    if [ "$PREV" != "$CURRENT" ];then
+    if [[ "$PREV" != "$CURRENT" ]];then
         echo "$CURRENT - $(du -sh $DEST/$CURRENT.tgz | awk '{print $1}') *"
-        for bak in $(cat $DEST/$CURRENT.index); do
+        for bak in $(cat $DEST/$CURRENT.index);do
             echo "$bak - $(du -sh $DEST/$bak.tgz | awk '{print $1}')"
         done
     fi
@@ -235,36 +235,49 @@ remove_backup(){
     # the main previous backup when index file is empty.
     #
     local OLD=$(awk 'NR==1{print}' $DEST/$PREV.index)
-    if [ ! -z "$OLD" ];then
-        rm $DEST/$OLD.tgz
-        rm $DEST/$OLD.snar
-        echo "Removed old backup [$OLD]"
-        echo "Updating index file"
-        sed -i '1d' $DEST/$PREV.index
+    if [[ ! -z "$OLD" ]];then
+        rm $DEST/$OLD.*
+        if [[ ! -f "$DEST/$OLD.tgz" ]];then
+            echo "Removed backup [$OLD]"
+            echo "Updating index file"
+            sed -i '1d' $DEST/$PREV.index
+        else
+            echo "Could not remove [$OLD]"
+        fi
     fi
     local OLD=$(awk 'NR==1{print}' $DEST/$PREV.index)
-    if [ -z "$OLD" ];then
-        rm $DEST/$PREV.tgz
-        rm $DEST/$PREV.snar
-        rm $DEST/$PREV.index
-        echo "Removed old backup [$PREV]"
-        PREV=$CURRENT
-        update_config
+    if [[ -z "$OLD" ]];then
+        rm $DEST/$PREV.*
+        if [[ ! -f "$DEST/$PREV.tgz" ]];then
+            echo "Removed backup [$PREV]*"
+            PREV=$CURRENT
+            update_config
+        else
+            echo "Could not remove [$PREV]*"
+        fi
     fi
 }
 
 cleanup_dest(){
     # Remove all old backups that are not present in CURRENT or PREV
-    for index in $(ls $DEST | grep .index);do
-        if [ "$index" != "$CURRENT.index" ] && [ "$index" != "$PREV.index" ];then
-            local NAME=${index%.*}
-            echo "Cleaning up $DEST - [$NAME]"
-            for file in $(cat $DEST/$index);do
-                rm $DEST/$file*
-            done
-            rm $DEST/$NAME*
+    echo "Cleaning up $DEST"
+    for tgz in $(ls $DEST | grep .tgz);do
+        if [[ ! -f "$DEST/$CURRENT.index" ]] || [[ ! -f "$DEST/$CURRENT.index" ]];then
+            echo "Missing index file; cleanup aborted!"
+            break
+        fi
+        if [[ $(cat $DEST/$CURRENT.index) != *${tgz%.*}* ]] && [[ "$tgz" != "$CURRENT.tgz" ]];then
+            if [[ $(cat $DEST/$PREV.index) != *${tgz%.*}* ]] && [[ "$tgz" != "$PREV.tgz" ]];then
+                rm $DEST/${tgz%.*}.*
+                if [[ ! -f "$DEST/$tgz" ]];then
+                    echo "Removed old backup [$tgz]"
+                else
+                    echo "Could not remove old backup [$tgz]"
+                fi
+            fi
         fi
     done
+    echo "Done."
 }
 
 calc_space(){
@@ -274,17 +287,17 @@ calc_space(){
     #
     cleanup_dest
     local TARGET_SIZE=$(du -s --block-size=1K $TARGET | awk '{print $1}')
-    if [ $(($TARGET_SIZE*2)) -gt $LIMIT ];then
+    if [[ $(($TARGET_SIZE*2)) -gt $LIMIT ]];then
         local FREE_SPACE=$(df --block-size=1K $DEST | awk '/\// {print $4}')
-        if [ $(($TARGET_SIZE*2)) -lt $FREE_SPACE ];then
-            if [ "$LIMIT" != "0" ];then
+        if [[ $(($TARGET_SIZE*2)) -lt $FREE_SPACE ]];then
+            if [[ "$LIMIT" != "0" ]];then
                 # Format output as human readable.
                 local NEW_LIMIT_GB=$(($(($(($TARGET_SIZE*2))/1024))/1024))
                 local NEW_LIMIT_MB=$(($(($TARGET_SIZE*2))/1024))
                 local NEW_LIMIT_KB=$(($TARGET_SIZE*2))
-                if [ $NEW_LIMIT_GB -ge 10 ];then
+                if [[ $NEW_LIMIT_GB -ge 10 ]];then
                     local NEW_LIMIT=$NEW_LIMIT_GB"GB"
-                elif [ $NEW_LIMIT_MB -ge 10 ];then
+                elif [[ $NEW_LIMIT_MB -ge 10 ]];then
                     local NEW_LIMIT=$NEW_LIMIT_MB"MB"
                 else
                     local NEW_LIMIT=$NEW_LIMIT_KB"KB"
@@ -299,9 +312,9 @@ calc_space(){
             echo "[WARNING] Low disk space; backup history might be limited!"
             local TEN=$(($(($TARGET_SIZE/100))*10))
             local MAX=$(($TARGET_SIZE+$TEN))
-            if [ $FREE_SPACE -le $MAX  ];then
+            if [[ $FREE_SPACE -le $MAX ]];then
                 echo "[CRITICAL] Disk space margin less than 10%; consider migrate backups!"
-            elif [ $FREE_SPACE -lt $TARGET_SIZE ];then
+            elif [[ $FREE_SPACE -lt $TARGET_SIZE ]];then
                 echo "[FAILED] Not enough disk space; exiting!"
                 exit 1
             fi
@@ -313,12 +326,12 @@ calc_space(){
     # when limit is set to 0.
     # Continue removing until space is sufficient.
     #
-    while true; do
+    while true;do
         local BACKUP_SIZE=$(du -s --block-size=1K $DEST | awk '{print $1}')
         local REQUIRED_SPACE=$(($BACKUP_SIZE+$TARGET_SIZE))
-        if [ "$LIMIT" != "0" ] && [ $REQUIRED_SPACE -gt $LIMIT ];then
+        if [[ "$LIMIT" != "0" ]] && [[ $REQUIRED_SPACE -gt $LIMIT ]];then
             remove_backup
-        elif [ "$LIMIT" == "0" ] && [ $REQUIRED_SPACE -ge $FREE_SPACE ];then
+        elif [[ "$LIMIT" == "0" ]] && [[ $REQUIRED_SPACE -ge $FREE_SPACE ]];then
             remove_backup
         else
             echo "Sufficient free space."
@@ -334,24 +347,24 @@ get_thresh(){
     # THRESH (default 70%) of the original full backup then
     # create a new full backup.
     #
-    if [ -z "$THRESH" ];then
+    if [[ -z "$THRESH" ]];then
         set_thresh 70
         update_config
     fi
-    if [ -z "$MAX" ];then
+    if [[ -z "$MAX" ]];then
         set_max 0
         update_config
     fi
-    if [ -f "$DEST/$CURRENT.index" ];then
+    if [[ -f "$DEST/$CURRENT.index" ]];then
         local LAST_BACKUP=$(cat $DEST/$CURRENT.index | tail -1)
-        if [ ! -z "$LAST_BACKUP" ];then
+        if [[ ! -z "$LAST_BACKUP" ]];then
             local LAST_BACKUP_SIZE=$(du -s --block-size=1K $DEST/$LAST_BACKUP.tgz | awk '{print $1}')
             local LAST_FULL_SIZE=$(du -s --block-size=1K $DEST/$CURRENT.tgz | awk '{print $1}')
             local THRESH_VAL=$(($(($LAST_FULL_SIZE*$THRESH))/100))
-            if [ "$MAX" != "0" ] && [ $(wc -l < "$DEST/$CURRENT.index") -ge $MAX ];then
+            if [[ "$MAX" != "0" ]] && [[ $(wc -l < "$DEST/$CURRENT.index") -ge $MAX ]];then
                 echo "MAX value ($MAX) reached."
                 NEW_FULL=1
-            elif [ $LAST_BACKUP_SIZE -ge $THRESH_VAL ];then
+            elif [[ $LAST_BACKUP_SIZE -ge $THRESH_VAL ]];then
                 echo "THRESH value ($THRESH%) reached."
                 NEW_FULL=1
             else
@@ -371,7 +384,7 @@ files_changed(){
     # If not, no backup is needed.
     #
     STATE=$(find $TARGET -type f -exec md5sum {} + | LC_ALL=C sort | md5sum | awk '{print $1}')
-    if [ ! -z "$HASH" ] && [ "$STATE" == "$HASH" ];then
+    if [[ ! -z "$HASH" ]] && [[ "$STATE" == "$HASH" ]];then
         echo "Hash match, nothing to backup."
         exit 0
     else
@@ -381,16 +394,16 @@ files_changed(){
 }
 
 create_backup(){
-    if [ ! -d "$TARGET" ];then
+    if [[ ! -d "$TARGET" ]];then
         echo "No target directory!"
         exit 1
-    elif [ ! -d "$DEST" ];then
+    elif [[ ! -d "$DEST" ]];then
         mkdir -pv $DEST
     fi
-    if [ -z "$CURRENT" ];then
+    if [[ -z "$CURRENT" ]];then
         CURRENT=$DATE
     fi
-    if [ -f "$DEST/$DATE.tgz" ];then
+    if [[ -f "$DEST/$DATE.tgz" ]];then
         echo "Backup [$DATE.tgz] already exists."
         exit 0
     fi
@@ -399,7 +412,7 @@ create_backup(){
     calc_space
     get_thresh
 
-    if [ $NEW_FULL == "1" ];then
+    if [[ $NEW_FULL == "1" ]];then
         PREV=$CURRENT
         CURRENT=$DATE
         echo "Creating full backup [$DEST/$CURRENT.tgz]"
@@ -418,28 +431,28 @@ create_backup(){
 }
 
 restore_from_backup(){
-    if [ "$#" -lt 2 ];then
+    if [[ "$#" -lt 2 ]];then
         echo "Error: missing argument!"
         exit 2
     else
         RESTORE_DATE=$1
         RESTORE_DIR=$2
     fi
-    if [ ! -d "$RESTORE_DIR" ];then
+    if [[ ! -d "$RESTORE_DIR" ]];then
         mkdir -pv $RESTORE_DIR
     fi
     
-    if [ "$RESTORE_DATE" == "$PREV" ] || $(grep -Fxq "$RESTORE_DATE" $DEST/$PREV.index);then
+    if [[ "$RESTORE_DATE" == "$PREV" ]] || $(grep -Fxq "$RESTORE_DATE" $DEST/$PREV.index);then
         echo "Unpacking $PREV ..."
         tar -xf $DEST/$PREV.tgz -g /dev/null -C $RESTORE_DIR
-        if [ "$RESTORE_DATE" != "$PREV" ];then
+        if [[ "$RESTORE_DATE" != "$PREV" ]];then
             echo "Restoring from backup $RESTORE_DATE ..."
             tar -xf $DEST/$RESTORE_DATE.tgz -g /dev/null -C $RESTORE_DIR
         fi
-    elif [ "$RESTORE_DATE" == "$CURRENT" ] || $(grep -Fxq "$RESTORE_DATE" $DEST/$CURRENT.index);then
+    elif [[ "$RESTORE_DATE" == "$CURRENT" ]] || $(grep -Fxq "$RESTORE_DATE" $DEST/$CURRENT.index);then
         echo "Unpacking $CURRENT ..."
         tar -xf $DEST/$CURRENT.tgz -g /dev/null -C $RESTORE_DIR
-        if [ "$RESTORE_DATE" != "$CURRENT" ];then
+        if [[ "$RESTORE_DATE" != "$CURRENT" ]];then
             echo "Restoring from backup $RESTORE_DATE ..."
             tar -xf $DEST/$RESTORE_DATE.tgz -g /dev/null -C $RESTORE_DIR
         fi
@@ -452,7 +465,7 @@ restore_from_backup(){
     exit 0
 }
 
-if [ "$#" -gt 3 ] && [ "$1" == "--restore" ] || [ "$#" -gt 2 ];then
+if [[ "$#" -gt 3 ]] && [[ "$1" == "--restore" ]] || [[ "$#" -gt 2 ]];then
     echo "To many arguments."
     exit 1
 fi
@@ -468,7 +481,7 @@ case $1 in
         list_backups
         ;;
     --log)
-        journalctl -r -u bakup-auto
+        journalctl -e -u bakup-auto
         ;;
     --restore)
         restore_from_backup $2 $3
